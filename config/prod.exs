86844d06f96cf7a9f02de3534c7f6a44dd1e586f
Eugene Maslenkov <yujiriwc@gmail.com>
2020-02-25 17:32:03 +0300

    chore: try to disable cache_static_manifest

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :battle_ground_web, BattleGroundWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "example.com", port: 80]#,
+#  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
 #
