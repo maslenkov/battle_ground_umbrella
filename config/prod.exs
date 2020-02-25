d92e0ffbd5f94b236695b2b5cd5f46f39bd6eb8b
Eugene Maslenkov <yujiriwc@gmail.com>
2020-02-25 17:45:49 +0300

    chore: update config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :battle_ground_web, BattleGroundWeb.Endpoint,
-  url: [host: "example.com", port: 80]#,
+  url: [scheme: "https", host: "elixir-cc-hero-v1.herokuapp.com", port: 443]#,
 #  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
