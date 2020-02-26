fe52a84ed28ae263a1e408c7df2b02a22c0456ce
Eugene Maslenkov <yujiriwc@gmail.com>
2020-02-26 15:02:00 +0300

    chore: update config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :battle_ground_web, BattleGroundWeb.Endpoint,
 #  url: [scheme: "http", host: "localhost", port: 4000],
   url: [scheme: "https", host: {:system, "HOST"}, port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
 #  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
